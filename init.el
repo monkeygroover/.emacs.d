06360e1d692b36e8e54f24878f65b49894067387
monkeygroover <richardbowker@monkeygroover.com>
2016-04-30 23:44:32 +0100

    stripped back, added EDTS

--- init.el
+++ init.el
@@ -1,54 +1,19 @@
+(require 'package)
 
-;; Enable IDO mode
-(require 'ido)
-(ido-mode t)
+(add-to-list 'package-archives '("elpa" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 
-(electric-indent-mode nil)
+(setq inhibit-startup-message t)
 
-; always use spaces, not tabs, when indenting
-(setq indent-tabs-mode nil)
+(setq inhibit-startup-echo-area-message t)
 
-; ignore case when searching
-(setq case-fold-search t)
+(setq show-trailing-whitespace t)
 
-; require final newlines in files when they are saved
-(setq require-final-newline t)
+(cua-mode)
 
-(setq vc-handled-backends ())
+(setq exec-path (append exec-path '("/usr/local/bin/")))
 
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
-
-(setq make-backup-files nil)
-
-(setq auto-save-default nil)
-
-(setq auto-save-file-name-transforms
-  `((".*" ,temporary-file-directory t))
-    backup-directory-alist `((".*" . ,temporary-file-directory)))
-
-(push "/usr/local/bin" exec-path)
-
-;; Imports
-;;--------------------------------------------------------------------------------;
-
-(load "~/.emacs.d/keys.el")
-(load "~/.emacs.d/packages.el")
-(load "~/.emacs.d/functions.el")
-(load "~/.emacs.d/ui.el")
-
-;; Modes
-(load "~/.emacs.d/modes/clojure.el")
-
-;; Mode initializers (used to associate file types with emacs modes)
-
-(add-to-list 'auto-mode-alist '("\\.less\\'" . css-mode))
-(add-to-list 'auto-mode-alist '("\\.scss\\'" . css-mode))
-
-(electric-indent-mode -1)
-
-;; Auto complete mode
-(require 'auto-complete)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
-(require 'auto-complete-config)
-(ac-config-default)
-(global-auto-complete-mode t)
+(add-hook 'after-init-hook 'my-after-init-hook)
+(defun my-after-init-hook ()
+  (require 'edts-start))
--- modes/clojure.el
+++
@@ -1,9 +0,0 @@
-;; Clojure
-;;--------------------------------------------------------------------------------;
-
-(eval-after-load "clojure-mode"
-  '(progn
-     (global-set-key (kbd "C-c C-j") 'cider-jack-in)))
-
-;; rainbow delimiters
-;; (global-rainbow-delimiters-mode)
